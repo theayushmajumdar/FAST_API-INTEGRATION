04b6244fcb97d842255aba592fdbaff675430012
Ayush Majumdar <theayushmajumdar@gmail.com>
2024-12-05 13:17:13 +0530

    Remove unwanted app/ directory from fast-api-image-ecs-deploy branch

--- app/config.py
+++
@@ -1,6 +0,0 @@
-class Settings:
-    MONGODB_URI: str = "mongodb+srv://connectayushmajumdar:1234@cluster0.x65xn.mongodb.net/fastAPI?retryWrites=true&w=majority&appName=Cluster0"
-    DATABASE_NAME: str = "fastAPI"
-    COLLECTION_NAME: str = "facollection"
-
-settings = Settings()
