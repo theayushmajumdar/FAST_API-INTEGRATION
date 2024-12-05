ee5d87ef34badff3a4173f3592124d10eeb5e75c
Ayush M <111861889+theayushmajumdar@users.noreply.github.com>
2024-12-05 16:07:19 +0530

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,5 +1,5 @@
 class Settings:
-    MONGODB_URI: str = "mongodb+srv://connectayushmajumdar:1234@cluster0.x65xn.mongodb.net/fastAPI?retryWrites=true&w=majority&appName=Cluster0"
+    MONGODB_URI: str = 
     DATABASE_NAME: str = "fastAPI"
     COLLECTION_NAME: str = "facollection"
 
